1aca15aac874e870c576bf2dc514520c17b69d96
alyonafuria <alyonatammur@gmail.com>
2025-10-23 00:19:47 +0200

    feat: miniapp manifest and config

---
+++ sunsettings_base/src/app/.well-known/route.ts
@@ -0,0 +1,42 @@
+// function withValidProperties(properties: Record<string, undefined | string | string[]>) {
+// return Object.fromEntries(
+//     Object.entries(properties).filter(([_, value]) => (Array.isArray(value) ? value.length > 0 : !!value))
+// );
+// }
+
+export async function GET() {
+return Response.json({
+  "accountAssociation": {
+    "header": "",
+    "payload": "",
+    "signature": ""
+  },
+  "baseBuilder": {
+    "allowedAddresses": ["0xB5592f774757Dc60c1601895e8E339C50916A563"]
+  },
+  "miniapp": {
+    "version": "1",
+    "name": "sunsettings",
+    "homeUrl": "https://catch.sunsettings.app",
+    "iconUrl": "https://catch.sunsettings.app/icon.png",
+    "splashImageUrl": "https://catch.sunsettings.app/name.png",
+    "splashBackgroundColor": "#009bfa",
+    "webhookUrl": "https://catch.sunsettings.app/api/webhook",
+    "subtitle": "we provide access to basic miracles",
+    "description": "get prediction of sunset beauty, share your photos with the world and track sunset catching progress",
+    "screenshotUrls": [
+      "https://catch.sunsettings.app/screenshot1.png",
+      "https://catch.sunsettings.app/screenshot2.png",
+      "https://catch.sunsettings.app/screenshot3.jpg"
+    ],
+    "primaryCategory": "social",
+    "tags": ["sunset", "miniapp", "baseapp", "photos", "sunsets", "nature", "beauty", "miracles", "social", "sunrise"],
+    "heroImageUrl": "https://catch.sunsettings.app/icon.png",
+    "tagline": "we provide access to basic miracles",
+    "ogTitle": "sunsettings",
+    "ogDescription": "get prediction of sunset beauty, share your photos with the world and track sunset catching progress",
+    "ogImageUrl": "https://catch.sunsettings.app/icon.png",
+    "noindex": true
+  }
+});
+}
